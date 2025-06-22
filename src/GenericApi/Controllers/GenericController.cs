4243043dd13def0e9b4f283f198bdc21359fae24
Tristan Zündorff <tzuendorff@gmail.com>
2025-06-22 21:18:26 +0200

    fix: change types to match new Model type for ID in Controller

--- src/GenericApi/Controllers/GenericController.cs
+++ src/GenericApi/Controllers/GenericController.cs
@@ -24,21 +24,6 @@ namespace GenericApi.Controllers
             { HttpStatusCode.InternalServerError, "Internal server error" },
             { HttpStatusCode.NotFound, "Could not find order" },
         };
-
-        // Example Order. Delete later.
-        private Order exampleOrder = new Order
-        {
-            Id = 123,
-            CustomerFirstName = "John",
-            CustomerLastName = "Smith",
-            Approved = true,
-            Items = new List<Item> {
-                new Item {
-                    Id = 456,
-                    Amount = 5 }
-            }
-        };
-
         [HttpPost ("/orders")]
         public IActionResult CreateOrder(Order order)
         {
@@ -55,9 +40,10 @@ namespace GenericApi.Controllers
 
 
         [HttpGet("/orders")]
-        public IActionResult GetOrders(int? orderId)
+        public IActionResult GetOrders(string? orderId)
         {
-            if (!orderId.HasValue)
+            // Empty filter. Return all orders in collection
+            if (orderId == null || orderId == "")
             {
                 try
                 {
@@ -69,16 +55,19 @@ namespace GenericApi.Controllers
                     return StatusCode((int)HttpStatusCode.InternalServerError, ErrorText[HttpStatusCode.InternalServerError]);
                 }
             }
+
+            // Non empty filter. Return all orders whose Id contains the given orderId.
             try
             {
-                var resultOrder = _businessLogic.ReadOneEntity(orderId.Value);
+                var resultOrders = _businessLogic.ReadAllEntitiesByFilter(orderId);
 
-                if(resultOrder == null)
+                if (resultOrders == null)
                 {
+                    _logger.LogError($"No order found whose orderId containes {orderId}");
                     return StatusCode((int)HttpStatusCode.NotFound, ErrorText[HttpStatusCode.NotFound]);
                 }
 
-                return Ok(_businessLogic.ReadOneEntity(orderId.Value));
+                return Ok(resultOrders);
             }
             catch (Exception exception)
             {
@@ -87,44 +76,44 @@ namespace GenericApi.Controllers
             }
         }
 
-        [HttpPut("/orders")]
-        public IActionResult UpdateOrder(Order order)
-        {
-            try
-            {
-                _businessLogic.UpdateEntity(order);
-                return Ok();
-            }
-            catch (KeyNotFoundException exception)
-            {
-                _logger.LogError($"Could not find order with id {order.Id}. {exception}");
-                return StatusCode((int)HttpStatusCode.NotFound, ErrorText[HttpStatusCode.NotFound]);
-            }
-            catch (Exception exception)
-            {
-                _logger.LogError($"Could not update order. {exception}");
-                return StatusCode((int)HttpStatusCode.InternalServerError, ErrorText[HttpStatusCode.InternalServerError]);
-            }
-        }
+        //[HttpPut("/orders")]
+        //public IActionResult UpdateOrder(Order order)
+        //{
+        //    try
+        //    {
+        //        _businessLogic.UpdateEntity(order);
+        //        return Ok();
+        //    }
+        //    catch (KeyNotFoundException exception)
+        //    {
+        //        _logger.LogError($"Could not find order with id {order.Id}. {exception}");
+        //        return StatusCode((int)HttpStatusCode.NotFound, ErrorText[HttpStatusCode.NotFound]);
+        //    }
+        //    catch (Exception exception)
+        //    {
+        //        _logger.LogError($"Could not update order. {exception}");
+        //        return StatusCode((int)HttpStatusCode.InternalServerError, ErrorText[HttpStatusCode.InternalServerError]);
+        //    }
+        //}
 
-        [HttpDelete("/orders")]
-        public IActionResult DeleteOrders(int orderId)
-        {
-            try
-            {
-                _businessLogic.DeleteEntity(orderId);
-                return Ok();
-            }
-            catch (KeyNotFoundException exception)
-            {
-                _logger.LogError($"Could not find order with id {orderId}. {exception}");
-                return StatusCode((int)HttpStatusCode.NotFound, ErrorText[HttpStatusCode.NotFound]);
-            }
-            catch (Exception exception)
-            {
-                _logger.LogError($"Could not delete order with id {orderId}. {exception}");
-                return StatusCode((int)HttpStatusCode.InternalServerError, ErrorText[HttpStatusCode.InternalServerError]);
-            }
-        }
+        //[HttpDelete("/orders")]
+        //public IActionResult DeleteOrders(int orderId)
+        //{
+        //    try
+        //    {
+        //        _businessLogic.DeleteEntity(orderId);
+        //        return Ok();
+        //    }
+        //    catch (KeyNotFoundException exception)
+        //    {
+        //        _logger.LogError($"Could not find order with id {orderId}. {exception}");
+        //        return StatusCode((int)HttpStatusCode.NotFound, ErrorText[HttpStatusCode.NotFound]);
+        //    }
+        //    catch (Exception exception)
+        //    {
+        //        _logger.LogError($"Could not delete order with id {orderId}. {exception}");
+        //        return StatusCode((int)HttpStatusCode.InternalServerError, ErrorText[HttpStatusCode.InternalServerError]);
+        //    }
+        //}
     }
 }
