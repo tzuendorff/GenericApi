d59ed1b540cbe00af91b4d734045fb7ebfe525a6
Tristan Zündorff <tzuendorff@gmail.com>
2025-06-22 21:16:26 +0200

    chore: renames Classes folder to Models

---
+++ src/GenericApi/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+
+namespace GenericApi.Classes;
+
+public class Order 
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+    public required string CustomerFirstName { get; set; }
+    public required string CustomerLastName { get; set; }
+    public required bool Approved { get; set; }
+    public required List<Item> Items { get; set; }
+}
